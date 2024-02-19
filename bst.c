d9b6401626391693207defa8d7b18ca5f55b6782
Harshitha <harshithams016@gmail.com>
2024-02-19 21:52:46 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -13,8 +13,8 @@ typedef struct node *NODE;
 NODE getnode()
 {
 	NODE x;
-	x = (NODE)malloc(sizeof(struct node));
-	if(x == NULL)
+	x=(NODE)malloc(sizeof(struct node));
+	if(x==NULL)
 	{
 		printf("Insufficient memory");
 		exit(0);
@@ -24,93 +24,99 @@ NODE getnode()
 
 void preorder(NODE root)
 {
-	if(root == NULL) return;
-	printf("%d\t", root->info);
+	if(root==NULL) return;
+	printf("%d\t",root->info);
 	preorder(root->llink);
 	preorder(root->rlink);
 }
 
 void postorder(NODE root)
 {
-	if(root == NULL) return;
+	if(root==NULL) return;
 	postorder(root->llink);
 	postorder(root->rlink);
-	printf("%d\t", root->info);
+	printf("%d\t",root->info);
 }
 
 void inorder(NODE root)
 {
-	if(root == NULL) return;
+	if(root==NULL) return;
 	inorder(root->llink);
-	printf("%d\t", root->info);
+	printf("%d\t",root->info);
 	inorder(root->rlink);
 }
 
-NODE insert(int item, NODE root)
+NODE insert(int item,NODE root)
 {
-	NODE temp, cur, prev;
-	temp = getnode();
-	temp->info = item;
-	temp->llink = NULL;
-	temp->rlink = NULL;
-	if(root == NULL)
+	NODE temp,cur,prev;
+	temp=getnode();
+	temp->info=item;
+	temp->llink=NULL;
+	temp->rlink=NULL;
+	if(root==NULL)
 		return temp;
-	prev = NULL;
-	cur = root;
-	while(cur != NULL)
+	prev=NULL;
+	cur=root;
+	while(cur!=NULL)
 	{
-		prev = cur;
-		if(item < cur->info)
-			cur = cur->llink;
-		else
-			cur = cur->rlink;
+		prev=cur;
+	    	if(item==cur->info)
+		{
+			printf("Duplicate item are not allowed\n");
+			free(temp);
+			return root;
+		}
+		if(item<cur->info)	
+                 cur=cur->llink;
+		    else			
+                 cur=cur->rlink;
 	}
-	if(item < prev->info)
-		prev->llink = temp;
+	if(item<prev->info)
+		prev->llink=temp;
 	else
-		prev->rlink = temp;
+		prev->rlink=temp;
 	return root;
 }
 
 NODE search(int item, NODE root)
 {
 	NODE cur;
-	if(root == NULL) return NULL;
-	cur = root;
-	while(cur != NULL)
+	if(root==NULL) return NULL;
+	cur=root;
+	while(cur!=NULL)
 	{
-		if(item == cur->info) return cur;
-		if(item < cur->info)
-			cur = cur->llink;
+		if(item==cur->info) 
+              return cur;
+		if(item<cur->info)
+			cur=cur->llink;
 		else
-			cur = cur->rlink;
+			cur=cur->rlink;
 	}
 	return NULL;
 }
 
 void main()
 {
-	int item, ch, n, i;
-	NODE root, cur;
-	root = NULL;
+	int item,ch,n,i;
+	NODE root,cur;
+	root=NULL;
 	while (1)
 	{
-		printf("\nEnter the choice\n1.Insert\n2.Preorder\n3.Inorder\n4.Postorder\n5.Search an Element\n6:Exit\n");
-		scanf("%d", &ch);
+printf("\nEnter thechoice\n1.Insert\n2.Preorder\n3.Inorder\n4.Postorder\n5.Search an Element\n6:Exit\n");
+		scanf("%d",&ch);
 		switch(ch)
 		{
 			case 1:
 				printf("Enter the number of items\n");
-				scanf("%d", &n);
-				printf("Enter the items to be inserted\n");
-				for(i = 0; i < n; i++)
+				scanf("%d",&n);
+				printf("Enter the item to be inserted\n");
+				for(i=0;i<n; i++)
 				{
-					scanf("%d", &item);
-					root = insert(item, root);
+				scanf("%d",&item);
+				root=insert(item,root);
 				}
 				break;
-			case 2:
-				if(root == NULL)
+			case 2: if(root==NULL)
 				{
 					printf("Tree is empty\n");
 					break;
@@ -119,8 +125,7 @@ void main()
 				preorder(root);
 				printf("\n");
 				break;
-			case 3:
-				if(root == NULL)
+			case 3: if(root==NULL)
 				{
 					printf("Tree is empty\n");
 					break;
@@ -129,8 +134,7 @@ void main()
 				inorder(root);
 				printf("\n");
 				break;
-			case 4:
-				if(root == NULL)
+			case 4: if(root==NULL)
 				{
 					printf("Tree is empty\n");
 					break;
@@ -139,19 +143,16 @@ void main()
 				postorder(root);
 				printf("\n");
 				break;
-			case 5:
-				printf("Enter the item to be searched\n");
-				scanf("%d", &item);
-				cur = search(item, root);
-				if(cur == NULL)
-					printf("Item not found\n");
-				else
-					printf("Item found\n");
+			case 5:	printf("Enter the item to be searched\n");
+					scanf("%d",&item);
+					cur=search(item,root);
+					if(cur==NULL)
+						printf("Item not found\n");
+					else
+						printf("Item found\n");
 				break;
-			case 6:
-				exit(0);
-			default:
-				printf("Enter valid choice\n");
+			case 6: exit(0);
+			default: printf("Enter valid choice\n");
 		}
 	}
 }
